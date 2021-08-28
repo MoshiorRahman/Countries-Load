ba319db0f1633c98afe9c63d29d4f6be102a269f
Moshior <moshiorrahman279312@gmail.com>
2021-08-28 22:01:39 +0600

    last commit

--- js/countries.js
+++ js/countries.js
@@ -8,25 +8,34 @@ const displayCountries = countries => {
     // for (const country of countries) {
     //     console.log(country)
     // }
-    console.log(countries)
     const countriesDiv = document.getElementById('countries');
     countries.forEach(country => {
-        console.log(country)
         const div = document.createElement('div');
         div.classList.add('country')
-        const h2 = document.createElement('h2');
-        h2.style.color = "indigo"
-        h2.innerText = `name:${country.name}`;
-        div.appendChild(h2);
-        const h3 = document.createElement('h3');
-        h3.style.color = "BlueViolet";
-        h3.innerText = `Capital:${country.capital}`;
-        div.appendChild(h3)
-        const h4 = document.createElement('h4');
-        h4.style.color = "Crimson";
-        h4.innerText = `Language:${country.languages.length}`
-        div.appendChild(h4);
+        div.innerHTML = `
+            <h3>Name:${country.name}</h3>
+            <p>language:${country.capital}</p>
+            <h4>language quantity:${country.languages.length}</h4>
+            <button onclick="loadCountryByName('${country.name}')">Details</button>
+        `
 
         countriesDiv.appendChild(div);
     });
+
+}
+const loadCountryByName = name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountrieDetail(data[0]))
+}
+
+const displayCountrieDetail = country => {
+    console.log(country)
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv.innerHTML = `
+        <h4>${country.name}</h4>
+        <p>population: ${country.population}</p>
+        <img width=150px src="${country.flag}">
+    `
 }
